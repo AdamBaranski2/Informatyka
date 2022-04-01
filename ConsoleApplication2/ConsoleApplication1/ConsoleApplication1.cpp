20f9121c58aa4f2e648e93f51873f93f36c01d87
AdamBaranski2 <chomik126@gmail.com>
2022-04-01 10:07:47 +0200

    xD

---
+++ ConsoleApplication2/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   int a = 0;
+   int b = 0;
+
+   cout << "podaj liczbe" << endl;;
+   cin >> a ;
+   cout << "podaj druga liczbe" << endl;
+   cin >> b ;
+   
+   while (a != b)
+   {
+
+	   if (a > b) {
+
+		   a -= b;
+
+
+
+
+	   }
+
+	   else
+		   b -= a;
+   }
+   int NWD = a;
+   cout<<"NWD: " << NWD;
+   cout << "NWW: " << NWD/(a*b);
+}
+
+
+
