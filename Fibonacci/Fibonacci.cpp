c1cb235f24490f5e98b8c74790aef02ead58bc96
AdamBaranski2 <chomik126@gmail.com>
2022-05-11 10:48:43 +0200

    fibonacci

---
+++ Fibonacci/Fibonacci.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+
+void ciag( int n) {
+
+
+	long long a = 0, b = 1;
+	for (int i = 0; i < n;i++) {
+		cout << b;
+		b += a;
+
+		a = b-a;
+
+
+	}
+
+
+}
+
+int main()
+
+
+{ 
+	int n;
+	cout << "podaj dlugosc ciagu ";
+	cin >> n;
+	ciag (n);
+
+
+}
