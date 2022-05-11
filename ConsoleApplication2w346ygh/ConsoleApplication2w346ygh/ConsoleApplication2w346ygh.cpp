ec92e05268010d77dab3c6a09a045d16b22ea042
AdamBaranski2 <chomik126@gmail.com>
2022-05-11 10:36:39 +0200

    szyfr cezara

---
+++ ConsoleApplication2w346ygh/ConsoleApplication2w346ygh/ConsoleApplication2w346ygh.cpp
@@ -0,0 +1,51 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+
+
+
+string kod(string slowo, int klucz) {
+    
+    for (int i = 0; i < slowo.length(); i++) {
+        if ((int)slowo[i]==32) {
+        }
+        else {
+            slowo[i] = (char)((int)slowo[i] + klucz);
+        }
+    }
+
+
+  
+
+    return slowo;
+}
+string dekod(string slowo, int klucz) {
+
+    for (int i = 0; i < slowo.length(); i++) {
+        if ((int)slowo[i] == 32) {
+        }
+        else {
+            slowo[i] = (char)((int)slowo[i] - klucz);
+        }
+    }
+
+    return slowo;
+}
+
+
+int main(){
+    cout << "podaj slowo" << endl;
+    string slowo="";
+    getline(cin, slowo);
+
+    cout << "podaj klucz" << endl;
+    int klucz ;
+    cin >> klucz;
+
+    string kod1 = kod(slowo, klucz);
+
+    cout << kod1 << endl;
+
+    cout << dekod(kod1, klucz);
+}
+
